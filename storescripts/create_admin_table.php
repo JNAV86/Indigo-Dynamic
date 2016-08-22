8da10aa0941f17e5bc30533299ae8628500d9764
Joshua Cogburn <joshua.dfw86@gmail.com>
2016-08-22 01:25:58 -0400

    Initial Commit

---
+++ storescripts/create_admin_table.php
@@ -0,0 +1,21 @@
+<?php
+//http://www.joshcogburn.com
+// Written by Joshua Cogburn January 01, 2013
+
+// Connect to the MySQL database  
+require "connect_to_mysql.php";  
+
+$sqlCommand = "CREATE TABLE admin (
+		 		 id int(11) NOT NULL auto_increment,
+				 username varchar(24) NOT NULL,
+		 		 password varchar(24) NOT NULL,
+		 		 last_log_date date NOT NULL,
+		 		 PRIMARY KEY (id),
+		 		 UNIQUE KEY username (username)
+		 		 ) ";
+if (mysql_query($sqlCommand)){ 
+    echo "Your admin table has been created successfully!"; 
+} else { 
+    echo "CRITICAL ERROR: admin table has not been created.";
+}
+?>
